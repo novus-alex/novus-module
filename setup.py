53acd3d7ad5472693a99fe5e05ee7011db9a4d6f
Novus Alex <69415374+novus-alex@users.noreply.github.com>
2020-08-13 16:37:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ setup(
   author = 'novus-alex',
   author_email = 'alexandre@hachet.com',
   url = 'https://novussapce.inovaperf.me',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/novus-alex/novus-module/archive/0.1.tar.gz',
   keywords = ['NOVUS', 'SPACE', 'PROJECT'],
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[
+          'shutil',
+          'requests',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
