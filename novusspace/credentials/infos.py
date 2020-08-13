5fb95e5e03d5dae9b70f179afad6d01357bcc416
Novus Alex <69415374+novus-alex@users.noreply.github.com>
2020-08-13 16:27:22 +0200

    Add files via upload

---
+++ novusspace/credentials/infos.py
@@ -0,0 +1,18 @@
+#! Python
+# (C) 2020 Novus Space
+
+version = '1.0'
+author = 'novus-alex'
+company = 'Novus Space'
+
+
+def getVersion():
+    return version
+
+
+def getAuthor():
+    return author
+
+
+def getCompany():
+    return company
